c9edd8be3214022b7954e000e437ca602f4913a6
pppiyo <yueqinli@usc.edu>
2023-05-19 12:05:13 -0700

    nav-bar auto-feed adaptation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import logo from "../images/logo.svg"
+import { pageLinks, socialLinks } from "../data";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
@@ -10,44 +12,38 @@ const Navbar = () => {
                     </button>
                 </div>
                 <ul className="nav-links" id="nav-links">
-                    <li>
-                        <a href="#home" className="nav-link"> home </a>
-                    </li>
-
-                    <li>
-                        <a href="#about" className="nav-link"> about </a>
-                    </li>
-
-                    <li>
-                        <a href="#services" className="nav-link"> services </a>
-                    </li>
-
-                    <li>
-                        <a href="#tours" className="nav-link"> tours</a>
-                    </li>
+                    {pageLinks.map((link) => {
+                        return (<li key={link.id}>
+                            <a href={link.href} className="nav-link">
+                                {link.text}
+                            </a>
+                        </li>);
+                    })}
                 </ul>
 
                 <ul className="nav-icons">
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank"
-                            rel="noreferrer" className="nav-icon"
-                        ><i className="fab fa-facebook"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" className="nav-icon" rel="noreferrer"
-                        ><i className="fab fa-twitter"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank"
-                            rel="noreferrer" className="nav-icon"
-                        ><i className="fab fa-squarespace"></i
-                        ></a>
-                    </li>
+                    {socialLinks.map((link) => {
+                        const { id, href, icon } = link
+                        return (
+                            <li key={id} >
+                                <a
+                                    href={href}
+                                    target="_blank"
+                                    rel="noreferrer" className='nav-icon'
+                                >
+                                    <i className={icon}></i>
+                                </a>
+                            </li>
+                        );
+                    })}
+
+
                 </ul>
+
+
+
             </div>
-        </nav>
+        </nav >
     );
 }
 
